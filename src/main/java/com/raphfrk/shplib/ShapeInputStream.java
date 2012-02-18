e0bcf693a5ebc0edd07deff5e8fd381375b4cc92
Raphfrk <raphfrk@gmail.com>
2012-02-18 12:00:45 +0000

    initial version

---
+++ src/main/java/com/raphfrk/shplib/ShapeInputStream.java
@@ -0,0 +1,13 @@
+package com.raphfrk.shplib;
+
+/**
+ * Hello world!
+ *
+ */
+public class ShapeInputStream
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
